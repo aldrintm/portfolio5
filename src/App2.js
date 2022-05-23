7a8694db09de9a9ae7440c541cea5f0f011a3d6c
aldrintm <aldrin@plamarusa.com>
2022-05-22 23:16:56 -0700

    First Commit

---
+++ src/App2.js
@@ -0,0 +1,174 @@
+function App() {
+  return (
+    <div className=''>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <button className='btn btn-large'>ClickMe</button>
+      <br />
+      <input
+        type='text'
+        placeholder='Type here'
+        class='input input-bordered input-primary w-full max-w-xs'
+      />
+
+      <div className='container mx-auto'>
+        <div className='flex flex-wrap gap-4 p-4'>
+          <div className='flex-1 shrink py-4'>
+            <div className='card w-80 bg-base-100 shadow-xl'>
+              <figure>
+                <img
+                  src='https://api.lorem.space/image/shoes?w=400&h=225'
+                  alt='Shoes'
+                />
+              </figure>
+              <div class='card-body'>
+                <h2 class='card-title'>Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class='card-actions justify-end'>
+                  <button class='btn btn-primary'>Buy Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='flex-1 shrink py-4'>
+            <div className='card w-80 bg-base-100 shadow-xl'>
+              <figure>
+                <img
+                  src='https://api.lorem.space/image/shoes?w=400&h=225'
+                  alt='Shoes'
+                />
+              </figure>
+              <div className='card-body'>
+                <h2 className='card-title'>Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className='card-actions justify-end'>
+                  <button className='btn btn-primary'>Buy Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='flex-1 shrink py-4'>
+            <div className='card w-80 bg-base-100 shadow-xl'>
+              <figure>
+                <img
+                  src='https://api.lorem.space/image/shoes?w=400&h=225'
+                  alt='Shoes'
+                />
+              </figure>
+              <div className='card-body'>
+                <h2 className='card-title'>Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className='card-actions justify-end'>
+                  <button className='btn btn-primary'>Buy Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='flex-1 shrink py-4'>
+            <div className='card w-80 bg-base-100 shadow-xl'>
+              <figure>
+                <img
+                  src='https://api.lorem.space/image/shoes?w=400&h=225'
+                  alt='Shoes'
+                />
+              </figure>
+              <div className='card-body'>
+                <h2 className='card-title'>Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className='card-actions justify-end'>
+                  <button className='btn btn-primary'>Buy Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='flex-1 shrink py-4'>
+            <div className='card w-80 bg-base-100 shadow-xl'>
+              <figure>
+                <img
+                  src='https://api.lorem.space/image/shoes?w=400&h=225'
+                  alt='Shoes'
+                />
+              </figure>
+              <div className='card-body'>
+                <h2 className='card-title'>Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className='card-actions justify-end'>
+                  <button className='btn btn-primary'>Buy Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='flex-1 shrink py-4'>
+            <div className='card w-80 bg-base-100 shadow-xl'>
+              <figure>
+                <img
+                  src='https://api.lorem.space/image/shoes?w=400&h=225'
+                  alt='Shoes'
+                />
+              </figure>
+              <div className='card-body'>
+                <h2 className='card-title'>Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className='card-actions justify-end'>
+                  <button className='btn btn-primary'>Buy Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='flex-1 shrink py-4'>
+            <div className='card w-80 bg-base-100 shadow-xl'>
+              <figure>
+                <img
+                  src='https://api.lorem.space/image/shoes?w=400&h=225'
+                  alt='Shoes'
+                />
+              </figure>
+              <div className='card-body'>
+                <h2 className='card-title'>Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className='card-actions justify-end'>
+                  <button className='btn btn-primary'>Buy Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='flex-1 shrink py-4'>
+            <div className='card w-80 bg-base-100 shadow-xl'>
+              <figure>
+                <img
+                  src='https://api.lorem.space/image/shoes?w=400&h=225'
+                  alt='Shoes'
+                />
+              </figure>
+              <div className='card-body'>
+                <h2 className='card-title'>Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className='card-actions justify-end'>
+                  <button className='btn btn-primary'>Buy Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='flex-1 shrink py-4'>
+            <div className='card w-80 bg-base-100 shadow-xl'>
+              <figure>
+                <img
+                  src='https://api.lorem.space/image/shoes?w=400&h=225'
+                  alt='Shoes'
+                />
+              </figure>
+              <div className='card-body'>
+                <h2 className='card-title'>Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className='card-actions justify-end'>
+                  <button className='btn btn-primary'>Buy Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
